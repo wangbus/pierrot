01c5890b492f50cf0bcab098f5ed7845c0cd4a8b
James Wang <jw@slickage.com>
2013-07-05 17:41:49 -1000

    Initial instructions via optimist.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+var argv = require('optimist')
+  .usage('$0 -a <action>')
+  .demand('action')
+  .alias('a', 'action')
+  .describe('a', 'Perform an action')
+  .argv
+;
+
 console.log('aloha');
+
